3995573f9d25b493727e7610bb5f6d6b9348c84d
Nick Hoats <nhoatz@gmail.com>
2017-06-26 09:57:18 -0400

    finished 1st and 2nd iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,66 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+
+app.set('view engine', 'ejs');
+
+app.set('views', 'views');
+
+
+//layout stuff--------------------------------------------
+app.use(express.static('public'));
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.set('layout,', 'layout.ejs');
+//---------------------------------------------------------
+
+//ROUTES--------------------------------------------------
+app.get('/', (req, res, next) => {
+  res.render('home-views.ejs');
+});
+//stage 1 of random
+app.get('/random', (req, res, next) => {
+
+  client.getRandomJoke().then((jokeInfo) => {
+    let randJoke = jokeInfo.value;
+
+    res.render('random-jokes-views.ejs', {
+      myJoke: randJoke
+    });
+  });
+
+});
+
+app.get('/categories', (req, res, next) => {
+
+client.getJokeCategories()
+  .then((response)=>  {
+    // use the response here
+    res.render('categories.ejs',{
+      categoriesList: response
+    });
+
+  })
+  .catch((err)=> {
+    // handle error
+
+  });
+});
+
+app.get('/search', (req, res, next) => {
+  client.search(searchTerm)
+    .then((response) => {
+      // to stuff here
+      res.render('search-views.ejs', {
+        searchJoke: response
+      });
+    }).catch((err) => {
+      // handle error
+    });
+});
+
+//--------------------------------------------------------
+
+
+
+app.listen(3000);
